04a239fc1f8bc84ab1bc9f6f22a69f41264b9199
jackyhsuch <jackyhsu0102@hotmail.com>
2014-10-27 03:07:14 +0800

    add json lib

--- MMS/Storage.java
+++ MMS/Storage.java
@@ -1,5 +1,18 @@
-package gently.MMS;
+package gently.MMS;	
+
+//import org.json.JSONArray;
 
 public class Storage {
-
+	
+	private Storage() {
+		JSONObject IPtoLocationFile = new JSONOject();
+		JSONArray ServerFile = new JSONArray();
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 }
