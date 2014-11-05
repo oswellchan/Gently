eac119a2d4dd4df67190effc97297f3dfc77d8df
kiangkuang <kiangkuang@gmail.com>
2014-11-05 22:32:52 +0800

    Fixed dropdown bug on browse & search

--- wwwroot/search.php
+++ wwwroot/search.php
@@ -70,5 +70,8 @@ mysqli_close($conn);
 			</div>
 		</div>
 	</div>
+	<script
+		src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
