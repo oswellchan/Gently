2c13f80de74d0992d60dcd7ebda97d48cb081416
kuntat <kuntat@gmail.com>
2015-02-24 22:21:07 +0800

    Able to extract needed data from edge server

--- GentlyEdgeServer/EdgeTracker/src/EdgeTracker.java
+++ GentlyEdgeServer/EdgeTracker/src/EdgeTracker.java
@@ -4,25 +4,46 @@ import java.net.*;
 
 public class EdgeTracker {
 	public static final String STATS_FILE_LOCATION = "http://localhost/stat";	
-	public static final String NAME = "name";
+	public static final String BRACKETSLASH = "</";
+	//publishing and active should return boolean
+	//private static String[] flagArray = {"name", "bytes_in", "bytes_out", "address", "dropped", "publishing", "active", "width", "height", "frame_rate", "codec", "audio><codec", "nclients"};
+	private static String[] flagArray = {"name", "bytes_in", "bytes_out", "address", "dropped", "width", "height", "frame_rate", "codec", "audio><codec", "nclients"};
+	private static ArrayList<String> flagArList = new ArrayList<String>();
 	
 	public static void main (String args[]) {
 		BufferedReader br = null;
 		InputStream is = null;
 	    String line = null;
+	    Iterator<String> flagIter = null;
+	    flagArList.addAll(Arrays.asList(flagArray));
 	    
+	    //check if this is legit
 		EdgeTracker edgetracker = new EdgeTracker();
 		br = edgetracker.downloader(is, br);
 		
 		try {
 			while ((line = br.readLine()) != null) {
-				//find name of application
-				if (line.indexOf(NAME) > -1){
-					String temp = edgetracker.getString(line, NAME);
-					System.out.println(temp);
+				//cannot like that lah
+				if(line.indexOf("client")>-1 ){
+					if(line.indexOf("publishing")>-1)
+						System.out.println("This is a streamer");
+					else if(line.indexOf("active")>-1)
+						System.out.println("This is a viewer");
 				}
-					
-	    }
+				else{
+					//do nothing
+				}
+				flagIter = flagArList.iterator();
+				while(flagIter.hasNext()){
+					String flag = flagIter.next();
+					//System.out.println(flag);
+					if (line.indexOf(flag) > -1){
+						String temp = edgetracker.getString(line, flag);
+						System.out.println(flag+ " = " + temp);
+					}
+				}
+				
+			}	 
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
 		} finally {
@@ -52,6 +73,7 @@ public class EdgeTracker {
 	}
 	
 	private String getString(String line, String input) {
-		return line.substring(line.indexOf(input)+input.length()+1, line.indexOf("</"));
+		int startingIndex = line.indexOf(input);
+		return line.substring(startingIndex+input.length()+1, line.indexOf(BRACKETSLASH, startingIndex));
 	}
 }
