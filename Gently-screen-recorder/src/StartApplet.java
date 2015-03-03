d584f4c4416d124b4f7fb5115c6569778da73a0a
jackyhsuch <jackyhsu0102@hotmail.com>
2015-03-03 20:58:06 +0800

    Updated Recorder
    Still cannot get applet to work in browser, someone help!!!

--- Gently-screen-recorder/src/StartApplet.java
+++ Gently-screen-recorder/src/StartApplet.java
@@ -1,18 +1,48 @@
-import javax.swing.JApplet;
-import javax.swing.SwingUtilities;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class StartApplet extends JApplet {
+import javax.swing.JApplet;
+import javax.swing.JButton;
+
+@SuppressWarnings("serial")
+public class StartApplet extends JApplet implements ActionListener {
+	
+	static JButton control;
+	static Recorder recorder;
+
+	
+	
     //Called when this applet is loaded into the browser.
     public void init() {
         //Execute a job on the event-dispatching thread; creating this applet's GUI.
-        try {
-            SwingUtilities.invokeAndWait(new Runnable() {
-                public void run() {
-                    JRecorder jRecorder = new JRecorder();
-                }
-            });
-        } catch (Exception e) {
-            System.err.println("createGUI didn't complete successfully");
-        }
+    	//JRecorder jRecorder = new JRecorder();
+    	control = new JButton("Start Recording");
+    	control.setActionCommand("start");
+    	control.addActionListener(this);
+    	add(control);
+
     }
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("start")) {
+	    	
+	    	//start recording
+	    	recorder = new Recorder();
+	    	recorder.startRecording();
+	    	
+	    	System.out.println("START");
+	    	StartApplet.control.setActionCommand("stop");
+	    	StartApplet.control.setText("Stop Recording");
+	      
+	    } else if (e.getActionCommand().equals("stop")) {
+	    	
+	    	recorder.stopRecording();
+	    	recorder = null;
+	    	
+	    	System.out.println("STOP");
+	    	StartApplet.control.setText("Start Recording");
+	    	StartApplet.control.setActionCommand("start");
+	    }
+		
+	}
 }
