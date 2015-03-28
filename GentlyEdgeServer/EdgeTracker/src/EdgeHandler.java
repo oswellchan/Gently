8d9a45ac7e48ed1502d795205872043d6fb90b9f
kuntat <kuntat@gmail.com>
2015-03-28 15:42:36 +0800

    Handler calls Tracker for information every 5 sec

--- GentlyEdgeServer/EdgeTracker/src/EdgeHandler.java
+++ GentlyEdgeServer/EdgeTracker/src/EdgeHandler.java
@@ -6,6 +6,8 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -18,12 +20,37 @@ public class EdgeHandler {
 	public static final int PORTNUMBER = 9000;
 	public static final int EMPTYLINE = 0;
 
+	public static final int SECONDS = 5;
+	
 	//for logging
 	private final static Logger LOGGER = Logger.getLogger(EdgeHandler.class.getName());
 	private static Handler fh = null;
 
 	//Object to be sent to MMS
     private static EdgeServerTransferObject edgeTransferObject = new EdgeServerTransferObject();
+    
+    class ExtractTask extends TimerTask{
+    	EdgeTracker edgeTracker;
+    	ObjectOutputStream serverWriter;
+    	
+    	public ExtractTask(EdgeTracker edgeTracker, ObjectOutputStream serverWriter){
+    		this.edgeTracker = edgeTracker;
+    		this.serverWriter = serverWriter;
+    	}
+		public void run() {
+			edgeTransferObject = edgeTracker.extractData();
+			
+			try {
+				serverWriter.writeObject(edgeTransferObject);
+			} catch (IOException e) {
+				e.printStackTrace();
+				LOGGER.log(Level.SEVERE, e.toString());
+			}
+			
+			
+		}
+    	
+    }
     
 	public static void main (String args[]) {
 		try {
@@ -32,7 +59,7 @@ public class EdgeHandler {
 			LOGGER.setLevel(Level.SEVERE);
 			
 			EdgeTracker edgeTracker = new EdgeTracker();
-			
+
 			Socket s = new Socket(MMSURL, PORTNUMBER);
 
 			OutputStream os= s.getOutputStream();
@@ -42,26 +69,21 @@ public class EdgeHandler {
 			InputStreamReader isrServer = new InputStreamReader(s.getInputStream());
 			BufferedReader serverReader = new BufferedReader(isrServer);
 
+			//inFromUser and sentence to be used during later stage of implementation
 	        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 	        String sentence;
 	        sentence = inFromUser.readLine();
-	       
-	        // keep repeating until an empty line is read.
-			while (sentence.compareTo("") != EMPTYLINE) {
+	       						
+	        Timer timer = new Timer();
+	        EdgeHandler handler = new EdgeHandler();
+	        ExtractTask extract = handler.new ExtractTask(edgeTracker, serverWriter);
+	        timer.schedule(extract, 0, SECONDS * 1000);
 	        
-			   System.out.println(sentence);
-			   if (sentence.equals(EXTRACT)) {
-				   edgeTransferObject = edgeTracker.extractData();
-				   serverWriter.writeObject(edgeTransferObject);
-				   
-			   }
-	           sentence = inFromUser.readLine();
-	           
-	       }
-			serverWriter.writeBytes("\n");
-
+	        serverWriter.writeBytes("\n");
+	        
 			s.close();
 			fh.close();
+			
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 			LOGGER.log(Level.SEVERE, e.toString());
